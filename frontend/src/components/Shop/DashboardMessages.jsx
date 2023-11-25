3963a1d4f162a7e97c2c30df6f3557421bb87c77
Akintola Babatunde Rafiu <tundegabriel999@gmail.com>
2023-11-25 11:56:23 +0100

    made some changes

--- frontend/src/components/Shop/DashboardMessages.jsx
+++ frontend/src/components/Shop/DashboardMessages.jsx
@@ -9,7 +9,7 @@ import styles from "../../styles/styles";
 import { TfiGallery } from "react-icons/tfi";
 import socketIO from "socket.io-client";
 import { format } from "timeago.js";
-const ENDPOINT = " http://localhost:3000/";
+const ENDPOINT = " https://my-project-qdkw5a44w-iamfineboyygabriel.vercel.app";
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const DashboardMessages = () => {
