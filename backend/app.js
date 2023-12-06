da54976f9e682fc957ce8bbc3e4754f5679736c2
Akintola Babatunde Rafiu <tundegabriel999@gmail.com>
2023-12-07 00:47:05 +0100

    made changes

--- backend/app.js
+++ backend/app.js
@@ -98,7 +98,7 @@ const cors = require("cors");
 
 const allowCors = fn => async (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Origin', 'https://shopping-j2sb.vercel.app')
   // another common pattern
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
