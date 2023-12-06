bf258546c882972e929edefb30810aaf51a3f77b
Akintola Babatunde Rafiu <tundegabriel999@gmail.com>
2023-12-06 22:27:20 +0100

    made changes

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,75 @@
+// const express = require("express");
+// const ErrorHandler = require("./middleware/error");
+// const app = express();
+// const cookieParser = require("cookie-parser");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+
+// // Frontend link, deployed on Vercel
+// app.use(
+//   cors({
+//     origin: "https://shopping-j2sb.vercel.app/",
+//     credentials: true,
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     allowedHeaders: "Content-Type, Authorization",
+//   })
+// );
+// git;
+// app.use(cors(corsOptions));
+// // app.use(
+// //   cors({
+// //     origin: "http://localhost:3001",
+// //     credentials: true,
+// //   })
+// // );
+
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use("/test", (req, res) => {
+//   res.send("Hello world!");
+// });
+
+// app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+
+// // config
+// if (process.env.NODE_ENV !== "PRODUCTION") {
+//   require("dotenv").config({
+//     path: "config/.env",
+//   });
+// }
+
+// // import routes
+// const user = require("./controller/user");
+// const shop = require("./controller/shop");
+// const product = require("./controller/product");
+// const event = require("./controller/event");
+// const coupon = require("./controller/coupounCode");
+// const payment = require("./controller/payment");
+// const order = require("./controller/order");
+// const conversation = require("./controller/conversation");
+// const message = require("./controller/message");
+// const withdraw = require("./controller/withdraw");
+
+// app.use("/api/v2/user", user);
+// app.use("/api/v2/conversation", conversation);
+// app.use("/api/v2/message", message);
+// app.use("/api/v2/order", order);
+// app.use("/api/v2/shop", shop);
+// app.use("/api/v2/product", product);
+// app.use("/api/v2/event", event);
+// app.use("/api/v2/coupon", coupon);
+// app.use("/api/v2/payment", payment);
+// app.use("/api/v2/withdraw", withdraw);
+
+// // it's for ErrorHandling
+// app.use(ErrorHandler);
+
+// module.exports = app;
+
+
+
+
+
 const express = require("express");
 const ErrorHandler = require("./middleware/error");
 const app = express();
@@ -7,22 +79,20 @@ const cors = require("cors");
 
 // Frontend link, deployed on Vercel
 app.use(
-  cors({
-    origin: "https://shopping-j2sb.vercel.app/",
-    credentials: true,
-    optionsSuccessStatus: 200, // Some older browsers choke on 204
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "Content-Type, Authorization",
-  })
+  allowCors,
+  express.json(),
+  cookieParser(),
+  (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://shopping-j2sb.vercel.app/');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  },
+  bodyParser.urlencoded({ extended: true, limit: '50mb' })
 );
-git;
-app.use(cors(corsOptions));
-// app.use(
-//   cors({
-//     origin: "http://localhost:3001",
-//     credentials: true,
-//   })
-// );
+
+
 
 app.use(express.json());
 app.use(cookieParser());
@@ -66,3 +136,4 @@ app.use("/api/v2/withdraw", withdraw);
 app.use(ErrorHandler);
 
 module.exports = app;
+
